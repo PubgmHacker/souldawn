73e3920fb55ee3f8465050633919b48010bfc294
HellCart <hellcart@NoName.local>
2026-06-23 10:33:36 -0700

    Fix support bot: resolve config multiple IDs crash

--- souldawn-support-bot/config.py
+++ souldawn-support-bot/config.py
@@ -5,6 +5,6 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 WEBHOOK_PORT = int(os.getenv("WEBHOOK_PORT", "8080"))
 
-# Обработка списка ID операторов техподдержки
+# Безопасная обработка списка ID
 raw_ids = os.getenv("SUPPORT_CHAT_ID", "520904288,1195137911,8340654471,8735560311")
 SUPPORT_CHAT_IDS = [int(i.strip()) for i in raw_ids.split(",") if i.strip()]
